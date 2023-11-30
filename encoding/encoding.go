18a0df3b4b289438e49904055a1438bd5fd549fe
Tatiana Startseva <tatastarts@yandex.ru>
2023-11-30 12:32:39 +0300

    version 1

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,11 @@
 package encoding
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -23,18 +27,77 @@ type MyEncoder interface {
 	Encoding() error
 }
 
+func EmptyDockerCompose() models.DockerCompose {
+	var ports []string
+	var volumes []string
+	var links []string
+	web := models.Web{
+		Build:   "",
+		Ports:   ports,
+		Volumes: volumes,
+		Links:   links,
+	}
+	var environment []string
+	database := models.Database{
+		Image:       "",
+		Environment: environment,
+		Volumes:     volumes,
+	}
+	services := models.Services{
+		Web:      web,
+		Database: database,
+	}
+	dockerCompose := models.DockerCompose{
+		Version:  "3",
+		Services: services,
+	}
+	return dockerCompose
+}
+
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
-
+	var data []byte
+	data, err := os.ReadFile(j.FileInput)
+	if err != nil {
+		return err
+	}
+	dockerCompose := EmptyDockerCompose()
+	j.DockerCompose = &dockerCompose
+	err = json.Unmarshal(data, j.DockerCompose)
+	if err != nil {
+		return err
+	}
+	out, err := yaml.Marshal(j.DockerCompose)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(j.FileOutput, out, 0644)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	// Ниже реализуйте метод
-	// ...
-
+	var data []byte
+	data, err := os.ReadFile(y.FileInput)
+	if err != nil {
+		return err
+	}
+	dockerCompose := EmptyDockerCompose()
+	y.DockerCompose = &dockerCompose
+	err = yaml.Unmarshal(data, y.DockerCompose)
+	if err != nil {
+		return err
+	}
+	out, err := json.Marshal(y.DockerCompose)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(y.FileOutput, out, 0644)
+	if err != nil {
+		return err
+	}
 	return nil
 }
