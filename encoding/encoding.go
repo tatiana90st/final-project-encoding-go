9dc63a7d356f5e9553d761d259bf3bb5c81449c4
Tatiana Startseva <tatastarts@yandex.ru>
2023-12-06 19:05:17 +0300

    restoring original

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,11 +1,7 @@
 package encoding
 
 import (
-	"encoding/json"
-	"os"
-
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
-	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -27,77 +23,18 @@ type MyEncoder interface {
 	Encoding() error
 }
 
-func EmptyDockerCompose() models.DockerCompose {
-	var ports []string
-	var volumes []string
-	var links []string
-	web := models.Web{
-		Build:   "",
-		Ports:   ports,
-		Volumes: volumes,
-		Links:   links,
-	}
-	var environment []string
-	database := models.Database{
-		Image:       "",
-		Environment: environment,
-		Volumes:     volumes,
-	}
-	services := models.Services{
-		Web:      web,
-		Database: database,
-	}
-	dockerCompose := models.DockerCompose{
-		Version:  "3",
-		Services: services,
-	}
-	return dockerCompose
-}
-
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	var data []byte
-	data, err := os.ReadFile(j.FileInput)
-	if err != nil {
-		return err
-	}
-	dockerCompose := EmptyDockerCompose()
-	j.DockerCompose = &dockerCompose
-	err = json.Unmarshal(data, j.DockerCompose)
-	if err != nil {
-		return err
-	}
-	out, err := yaml.Marshal(j.DockerCompose)
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile(j.FileOutput, out, 0644)
-	if err != nil {
-		return err
-	}
+	// ниже реализуйте метод
+	// ...
+
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	var data []byte
-	data, err := os.ReadFile(y.FileInput)
-	if err != nil {
-		return err
-	}
-	dockerCompose := EmptyDockerCompose()
-	y.DockerCompose = &dockerCompose
-	err = yaml.Unmarshal(data, y.DockerCompose)
-	if err != nil {
-		return err
-	}
-	out, err := json.Marshal(y.DockerCompose)
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile(y.FileOutput, out, 0644)
-	if err != nil {
-		return err
-	}
+	// Ниже реализуйте метод
+	// ...
+
 	return nil
 }
